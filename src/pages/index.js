7b2023aa8f4c838f22776c912ffaf46f935d5bfc
AmoDinho <u14284783@tuks.co.za>
2021-03-04 22:41:03 +0200

    fixed eslit warning

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import HeadingOne from "../components/HeadingOne";
 /* eslint-disable import/no-webpack-loader-syntax */
 
 import Intro from "!babel-loader!@mdx-js/loader!../content/Intro.mdx";
